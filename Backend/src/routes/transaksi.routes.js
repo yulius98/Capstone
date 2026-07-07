4e14516c2f6f2e384fa4bbd4a6fda2245b403315
yulius k wijaya <70045649+yulius98@users.noreply.github.com>
2026-07-07 19:58:51 +0700

    fix bug

--- Backend/src/routes/transaksi.routes.js
+++ Backend/src/routes/transaksi.routes.js
@@ -4,7 +4,7 @@ const authMiddleware = require('../middlewares/auth.middleware');
 const authorizeRole = require('../middlewares/role.middleware');
 const { getAllTransaksi, getTransaksiByUser } = require('../controllers/transaksi.controller');
 
-router.get('/', authMiddleware, authorizeRole[2], getAllTransaksi);
+router.get("/", authMiddleware, authorizeRole([2]), getAllTransaksi);
 router.get('/user/:userId',authMiddleware, getTransaksiByUser);
 
 module.exports = router;
