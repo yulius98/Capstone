a0e13592c4cf26122d3ca865a2912600e1e44bfc
yulius k wijaya <70045649+yulius98@users.noreply.github.com>
2026-07-23 19:06:16 +0700

    fix : only BE

--- Backend/prisma/seed.js
+++ Backend/prisma/seed.js
@@ -10,25 +10,25 @@ const prisma = new PrismaClient({ adapter });
 
 async function main() {
   await prisma.userRole.createMany({
-    data: [
-      { role: 'User' },
-      { role: 'Admin' },
-    ],
+    data: [{ role: "User" }, { role: "Admin" }],
     skipDuplicates: true,
   });
 
   await prisma.jenisSampah.createMany({
     data: [
-      { kategori: 'plastik', hargaPerKg: 2000 },
-      { kategori: 'kertas', hargaPerKg: 1500 },
-      { kategori: 'logam', hargaPerKg: 5000 },
-      { kategori: 'organik', hargaPerKg: 8000},
-      { kategori: 'kaca', hargaPerKg: 6000}
+      { kategori: "plastik", hargaPerKg: 2000 },
+      { kategori: "kertas", hargaPerKg: 1500 },
+      { kategori: "logam", hargaPerKg: 5000 },
+      { kategori: "organik", hargaPerKg: 8000 },
+      { kategori: "kaca", hargaPerKg: 6000 },
+      { kategori: "elektronik", hargaPerKg: 1000 },
+      { kategori: "tekstil", hargaPerKg: 500 },
+      { kategori: "lainnya", hargaPerKg: 300 },
     ],
     skipDuplicates: true,
   });
 
-  const hashedPassword = await bcrypt.hash('Capstone123', 10);
+  const hashedPassword = await bcrypt.hash("Capstone123", 10);
 
   await prisma.user.upsert({
     where: { email: 'admin@pilahpinter.com' },
